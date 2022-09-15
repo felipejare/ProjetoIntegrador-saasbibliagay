a2c7dfcfa0cf500d5b77828f2e36ab83ebe704ec
Felipe "Felepopolo Jaré" Kuhl <99222712+felipejare@users.noreply.github.com>
2022-09-15 15:03:29 -0300

    Add files via upload

--- saasbiblia-main front end/src/components/templates/MenuS.vue
+++ saasbiblia-main front end/src/components/templates/MenuS.vue
@@ -1,13 +1,13 @@
 <script></script>
 
 <template>
-        <div class="menu">
-            <div class="menu-home"> <RouterLink to="/home"><h2>Home</h2></RouterLink></div>
-            <div class="menu-editoras"> <RouterLink to="/editoras"><h2>Editoras</h2></RouterLink></div>
-            <div class="menu-cat"> <RouterLink to="/categorias"><h2>Categorias</h2></RouterLink></div>
-            <div class="menu-aut"> <RouterLink to="/autores"><h2>Autores</h2></RouterLink></div>
-            <div class="menu-liv"><RouterLink to="/livros"><h2>Livros</h2></RouterLink></div>
-        </div>
+        <ul class="menu">
+            <li class="menu-home"> <RouterLink to="/home"><h2>Home</h2></RouterLink></li>
+            <li class="menu-editoras"> <RouterLink to="/editoras"><h2>Editoras</h2></RouterLink></li>
+            <li class="menu-cat"> <RouterLink to="/categorias"><h2>Categorias</h2></RouterLink></li>
+            <li class="menu-aut"> <RouterLink to="/autores"><h2>Autores</h2></RouterLink></li>
+            <li class="menu-liv"><RouterLink to="/livros"><h2>Livros</h2></RouterLink></li>
+        </ul>
 </template>
 
 <style>
@@ -16,6 +16,7 @@
   background-color: #773b80;
   height: inherit;
   width: 20%;
+  
 }
 
 .menu-home{
@@ -24,6 +25,8 @@
   margin: 10px 10px 10px 10px;
   color: white;
   border-radius: 15px;
+  text-decoration-line: none;
+  cursor: pointer;
 }
 .menu-editoras{
   background-color: #f9e5f0;
@@ -31,6 +34,7 @@
   margin: 10px 10px 10px 10px;
   color: white;
   border-radius: 15px;
+  text-decoration-line: none;
 }
 .menu-cat{
   background-color: #f9e5f0;
@@ -56,4 +60,28 @@
   border-radius: 15px;
 
 }
+h2{
+  text-decoration-line: none ;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+  width: 200px;
+  background-color: #f1f1f1;
+}
+
+li:hover {
+  background-color: #f7cde4;
+  color: white;
+}
+
+li {
+  display: block;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+
 </style>
